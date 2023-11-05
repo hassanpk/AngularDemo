6f121df7f7a6d9b279d0c623ba2a99ee4b0a45f1
Hassan.Nazeer <Hassan.Nazeer@LPTP-505>
2023-11-06 01:24:43 +0500

    Initial Code of Angular Demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddItemComponent } from './Pages/Products/add-item/add-item.component';
+import { ListItemsComponent } from './Pages/Products/list-items/list-items.component';
+import { EditItemComponent } from './Pages/Products/edit-item/edit-item.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { AuthGuardService } from './auth-guard';
+import { BarChartComponent } from './Pages/Charts/bar-chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {path :"", component: ListItemsComponent, canActivate:[AuthGuardService]},
+  {path :"login", component: LoginComponent },
+  {path: 'add', component:  AddItemComponent,canActivate:[AuthGuardService]},
+  {path : 'add/:id', component: AddItemComponent,canActivate:[AuthGuardService]},
+  {path: 'chart', component: BarChartComponent,canActivate:[AuthGuardService]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbCollapseModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './Pages/home/app.component';
+import { NavbarComponent } from './Pages/Shared/navbar/navbar.component';
+import { AddItemComponent } from './Pages/Products/add-item/add-item.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ListItemsComponent } from './Pages/Products/list-items/list-items.component';
+import { EditItemComponent } from './Pages/Products/edit-item/edit-item.component';
+
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { ProductService } from './Services/product-service.service';
+import { LoginComponent } from './Pages/login/login.component';
+import { SpinnerComponent } from './Pages/Shared/spinner/spinner.component';
+import { LoadingInterceptor } from './loading.interceptor';
+import { BarChartComponent } from './Pages/Charts/bar-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AddItemComponent,
+    ListItemsComponent,
+    EditItemComponent,
+    LoginComponent,
+    SpinnerComponent,
+    BarChartComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbCollapseModule,
+    NgbDropdownModule,
+    NgxDatatableModule
+  
   ],
-  providers: [],
+  providers: [ProductService,{
+    provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true
+  }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
